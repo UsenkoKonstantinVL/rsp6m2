4393ba6177fa750e887c8a7933366b3bb3b1a39d
KonstantinPC <kos16@mail.ru>
2016-12-04 14:47:26 +0300

    Create: buv_021

--- MainBlocks/rsp6m2/BlockForms/buv_021.Designer.cs
+++ MainBlocks/rsp6m2/BlockForms/buv_021.Designer.cs
@@ -35,11 +35,29 @@
             this.panel7 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.light3 = new rsp6m2.Controls.Light();
-            this.panel5 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.light1 = new rsp6m2.Controls.Light();
+            this.light2 = new rsp6m2.Controls.Light();
+            this.light4 = new rsp6m2.Controls.Light();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tumbler21 = new rsp6m2.Controls.Tumbler2();
+            this.tumbler22 = new rsp6m2.Controls.Tumbler2();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // voltmetr2
@@ -123,64 +141,247 @@
             this.light3.BackColor = System.Drawing.Color.Transparent;
             this.light3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light3.BackgroundImage")));
             this.light3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.light3.Location = new System.Drawing.Point(210, 279);
+            this.light3.Location = new System.Drawing.Point(210, 278);
             this.light3.Name = "light3";
             this.light3.Size = new System.Drawing.Size(50, 50);
             this.light3.TabIndex = 94;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.Transparent;
-            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel5.Location = new System.Drawing.Point(52, 284);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(45, 45);
-            this.panel5.TabIndex = 93;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(49, 243);
+            this.label8.Location = new System.Drawing.Point(22, 243);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(45, 15);
+            this.label8.Size = new System.Drawing.Size(104, 45);
             this.label8.TabIndex = 85;
-            this.label8.Text = "-300В";
+            this.label8.Text = "ВЫСОКОЕ \r\nНАПРЯЖЕНИЕ\r\n\r\n";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(215, 243);
+            this.label1.Location = new System.Drawing.Point(176, 243);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.Size = new System.Drawing.Size(115, 30);
             this.label1.TabIndex = 95;
-            this.label1.Text = "-300В";
+            this.label1.Text = "ТОК\r\nВЫПРЯМИТЕЛЯ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(372, 243);
+            this.label3.Location = new System.Drawing.Point(321, 243);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.Size = new System.Drawing.Size(155, 30);
             this.label3.TabIndex = 96;
-            this.label3.Text = "-300В";
+            this.label3.Text = "НАПРЯЖЕНИЕ\r\nНАКАЛА МАГНЕТРОНА";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = global::rsp6m2.Properties.Resources.fuse;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(58, 284);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox4.TabIndex = 97;
+            this.pictureBox4.TabStop = false;
+            // 
+            // light1
+            // 
+            this.light1.BackColor = System.Drawing.Color.Transparent;
+            this.light1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light1.BackgroundImage")));
+            this.light1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light1.Location = new System.Drawing.Point(367, 281);
+            this.light1.Name = "light1";
+            this.light1.Size = new System.Drawing.Size(50, 50);
+            this.light1.TabIndex = 98;
+            // 
+            // light2
+            // 
+            this.light2.BackColor = System.Drawing.Color.Transparent;
+            this.light2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light2.BackgroundImage")));
+            this.light2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light2.Location = new System.Drawing.Point(274, 424);
+            this.light2.Name = "light2";
+            this.light2.Size = new System.Drawing.Size(50, 50);
+            this.light2.TabIndex = 99;
+            // 
+            // light4
+            // 
+            this.light4.BackColor = System.Drawing.Color.Transparent;
+            this.light4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light4.BackgroundImage")));
+            this.light4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light4.Location = new System.Drawing.Point(374, 546);
+            this.light4.Name = "light4";
+            this.light4.Size = new System.Drawing.Size(50, 50);
+            this.light4.TabIndex = 100;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::rsp6m2.Properties.Resources.fuse;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(373, 641);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.TabIndex = 101;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::rsp6m2.Properties.Resources.fuse;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(379, 429);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox2.TabIndex = 102;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tumbler21
+            // 
+            this.tumbler21.BackColor = System.Drawing.Color.Transparent;
+            this.tumbler21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tumbler21.BackgroundImage")));
+            this.tumbler21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tumbler21.Location = new System.Drawing.Point(274, 551);
+            this.tumbler21.Name = "tumbler21";
+            this.tumbler21.Size = new System.Drawing.Size(45, 45);
+            this.tumbler21.TabIndex = 103;
+            // 
+            // tumbler22
+            // 
+            this.tumbler22.BackColor = System.Drawing.Color.Transparent;
+            this.tumbler22.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tumbler22.BackgroundImage")));
+            this.tumbler22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tumbler22.Location = new System.Drawing.Point(273, 641);
+            this.tumbler22.Name = "tumbler22";
+            this.tumbler22.Size = new System.Drawing.Size(45, 45);
+            this.tumbler22.TabIndex = 104;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(201, 345);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 105;
+            this.label4.Text = "ОГРАНИЧ.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(349, 345);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 30);
+            this.label5.TabIndex = 106;
+            this.label5.Text = "БЛОКИР.\r\nЗАМКНУТА\r\n";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(383, 484);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 107;
+            this.label6.Text = "10А";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(383, 694);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 108;
+            this.label7.Text = "5А";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(250, 477);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(104, 30);
+            this.label9.TabIndex = 109;
+            this.label9.Text = "ВЫСОКОЕ\r\nНАПРЯЖЕНИЕ\r\n";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(280, 533);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 15);
+            this.label10.TabIndex = 110;
+            this.label10.Text = "ВКЛ.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(266, 603);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 30);
+            this.label11.TabIndex = 111;
+            this.label11.Text = "ВЫКЛ.\r\nБОЛЬШЕ";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(264, 694);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 15);
+            this.label12.TabIndex = 112;
+            this.label12.Text = "МЕНЬШЕ";
             // 
             // buv_021
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(477, 567);
+            this.ClientSize = new System.Drawing.Size(477, 721);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tumbler22);
+            this.Controls.Add(this.tumbler21);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.light4);
+            this.Controls.Add(this.light2);
+            this.Controls.Add(this.light1);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.light3);
-            this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel7);
             this.Controls.Add(this.voltmetr3);
             this.Controls.Add(this.voltmetr2);
@@ -189,6 +390,9 @@
             this.Text = "buv_021";
             this.panel7.ResumeLayout(false);
             this.panel7.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -202,9 +406,24 @@
         private System.Windows.Forms.Panel panel7;
         private System.Windows.Forms.Label label2;
         private Controls.Light light3;
-        private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private Controls.Light light1;
+        private Controls.Light light2;
+        private Controls.Light light4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private Controls.Tumbler2 tumbler21;
+        private Controls.Tumbler2 tumbler22;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
